4d6e4c980f6abf5e51b6d1a62b1c027523686a18
VinishaVelthuru <45623617+VinishaVelthuru@users.noreply.github.com>
2024-09-19 15:00:54 +0530

    Add files via upload

---
+++ multiplication.javac.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+class multiplication
+{
+public static void main(string arg[])
+{
+int a[][]={{1,1,1},{2,2,2},{3,3,3}};
+int b[][]={{1,1,1},{2,2,2},{3,3,3}};
+for(int i=0; i<3; i++)
+{
+for(j=0; j<3; j++)
+{
+c[i][j]=0;
+for(int k=0; k<3; k++)
+{
+c=[i][j]*=a[i][k]*b[j][k];
+}
+system.out.println(c[i][j]+" ");
+}
+system.out.println();
+}
+}
+}
